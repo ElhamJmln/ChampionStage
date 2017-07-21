36ee38ed731b5837f4cba6cf3606639f3a8e4523
ElhamJmln <30336140+ElhamJmln@users.noreply.github.com>
2017-07-21 20:12:12 +0430

    Initial commit

---
+++ app/src/main/java/champion/stage/MainActivity.java
@@ -0,0 +1,13 @@
+package champion.stage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stage);
+    }
+}
